74bb5e38f5289c2fd0c8390832ba748a6764c4c3
alecyalcin <alecyalcin@gmail.com>
2025-04-16 15:45:19 -0300

    fix: atualizando criação de tabelas

--- tarefa_01/tarefa01-create.sql
+++ tarefa_01/tarefa01-create.sql
@@ -1,26 +1,33 @@
+-- Retirando tabelas
+
+DROP TABLE IF EXISTS funcionario;
+DROP TABLE IF EXISTS departamento;
+DROP TABLE IF EXISTS projeto;
+DROP TABLE IF EXISTS atividade;
+
 -- Habilitar o suporte a foreign keys
 PRAGMA foreign_keys = ON;
 
 -- Criando Tabelas
 
-CREATE TABLE IF NOT EXISTS funcionario(
+CREATE TABLE funcionario(
     codigo INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(50),
     sexo CHAR(1),
     dt_nasc DATE,
     salario DECIMAL(10,2),
     cod_depto INTEGER,
-    FOREIGN KEY (cod_depto) REFERENCES departamento(codigo)
+    FOREIGN KEY (cod_depto) REFERENCES departamento(codigo) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS departamento(
+CREATE TABLE departamento(
     codigo INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao VARCHAR(50),
     cod_gerente INTEGER,
-    FOREIGN KEY (cod_gerente) REFERENCES funcionario(codigo)
+    FOREIGN KEY (cod_gerente) REFERENCES funcionario(codigo) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS projeto(
+CREATE TABLE projeto(
     codigo INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(50),
     descricao VARCHAR(50),
@@ -28,16 +35,16 @@ CREATE TABLE IF NOT EXISTS projeto(
     cod_responsavel INTEGER,
     data_inicio DATE,
     data_fim DATE,
-    FOREIGN KEY (cod_depto) REFERENCES departamento(codigo),
-    FOREIGN KEY (cod_responsavel) REFERENCES funcionari(codigo)
+    FOREIGN KEY (cod_depto) REFERENCES departamento(codigo) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (cod_responsavel) REFERENCES funcionari(codigo) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS atividade(
+CREATE TABLE atividade(
     codigo INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(50),
     descricao VARCHAR(50),
     cod_responsavel INTEGER,
     data_inicio DATE,
     data_fim DATE,
-    FOREIGN KEY (cod_responsavel) REFERENCES funcionario(codigo)
+    FOREIGN KEY (cod_responsavel) REFERENCES funcionario(codigo) ON DELETE SET NULL ON UPDATE CASCADE
 );
